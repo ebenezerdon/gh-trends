ff725fb45094a47edd9be059e4b33e2cf16d2747
ebenezerdon <ebenezerdonu@gmail.com>
2022-01-29 22:11:18 +0100

    add Cards component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import useFetch from 'react-fetch-hook'
-import { db, addFav, removeFav } from './db'
+import { db } from './db'
 import './App.css'
 import { useLiveQuery } from 'dexie-react-hooks'
+import Cards from './components/Cards'
 
 function App() {
   const date = new Date()
@@ -13,21 +14,11 @@ function App() {
   const { data, isLoading, error } = useFetch(url)
 
   const favourites = useLiveQuery(() => db.favRepos?.toArray())
-  console.log('==fav==>', favourites)
 
   return (
     <div className="App">
       <h1>{formattedDate}</h1>
-      {data?.items?.map(repo => (
-        <div key={repo.id}>
-          <img src={repo.owner?.avatar_url} alt="" />
-          <h3>{repo.name}</h3>
-          <p>stars: {repo.stargazers_count}</p>
-          <button onClick={() => addFav(repo)}>Add to Favorites</button>
-          <button onClick={() => removeFav(repo)}>Remove from Favorites</button>
-          <a href={repo.html_url} target="_blank" rel="noreferrer">Open in GitHub</a>
-        </div>
-      ))}
+      <Cards data={data}/>
     </div>
   );
 }
---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import { addFav, removeFav } from '../db'
+
+const Cards = ({ data }) => {
+  return (
+    <div>
+      {data?.items?.map(repo => (
+        <div key={repo.id}>
+          <img src={repo.owner?.avatar_url} alt="" />
+          <h3>{repo.name}</h3>
+          <p>stars: {repo.stargazers_count}</p>
+          <button onClick={() => addFav(repo)}>Add to Favorites</button>
+          <button onClick={() => removeFav(repo)}>Remove from Favorites</button>
+          <a href={repo.html_url} target="_blank" rel="noreferrer">Open in GitHub</a>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Cards
